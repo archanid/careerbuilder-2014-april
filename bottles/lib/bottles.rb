d1789ad6be73a5e8a04045d88dc2f079189c2f2d
solostyle <spam@solostyle.net>
2014-04-28 14:41:32 -0500

    step

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -3,13 +3,13 @@ class Bottles
   def verse(i)
     case i
     when 3..99
-      "#{i} #{pluralize(i, container)} #{item_suffix} #{where}, #{i} #{pluralize(i, container)} #{item_suffix}.\nTake one down and pass it around, #{i-1} #{pluralize(i-1, container)} %s %s.\n" % [item_suffix, where]
+      "#{i} #{pluralize(i, container)} #{item_suffix} #{where}, #{i} #{pluralize(i, container)} #{item_suffix}.\nTake one down and pass it around, #{i-1} #{pluralize(i-1, container)} #{item_suffix} #{where}.\n"
     when 2
-      "2 #{pluralize(i, container)} #{item_suffix} #{where}, 2 #{pluralize(i, container)} #{item_suffix}.\nTake one down and pass it around, 1 #{pluralize(i-1, container)} %s %s.\n" % [item_suffix, where]
+      "2 #{pluralize(i, container)} #{item_suffix} #{where}, 2 #{pluralize(i, container)} #{item_suffix}.\nTake one down and pass it around, 1 #{pluralize(i-1, container)} #{item_suffix} #{where}.\n"
     when 1
-      "1 #{pluralize(i, container)} #{item_suffix} #{where}, 1 #{pluralize(i, container)} #{item_suffix}.\nTake it down and pass it around, no more #{pluralize(i-1, container)} %s %s.\n" % [item_suffix, where]
+      "1 #{pluralize(i, container)} #{item_suffix} #{where}, 1 #{pluralize(i, container)} #{item_suffix}.\nTake it down and pass it around, no more #{pluralize(i-1, container)} #{item_suffix} #{where}.\n"
     else
-      "No more #{pluralize(i, container)} #{item_suffix} #{where}, no more #{pluralize(i, container)} #{item_suffix}.\nGo to the store and buy some more, 99 #{pluralize(i-1, container)} %s %s.\n" % [item_suffix, where]
+      "No more #{pluralize(i, container)} #{item_suffix} #{where}, no more #{pluralize(i, container)} #{item_suffix}.\nGo to the store and buy some more, 99 #{pluralize(i-1, container)} #{item_suffix} #{where}.\n"
     end
   end
 
