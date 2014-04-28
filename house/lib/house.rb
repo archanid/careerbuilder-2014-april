25428b60741d90b6104a46d1eb5261e6fd079c94
solostyle <spam@solostyle.net>
2014-04-28 12:10:08 -0500

    step

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -4,9 +4,12 @@ class House
     1.upto(12).map {|i| line(i)}.join("\n")
   end
 
+  def line(i)
+      "This is %s.\n" % lines.last(i).join(" ")
+  end
+
   def lines
-    [
-      "the horse and the hound and the horn that belonged to", 
+    [ "the horse and the hound and the horn that belonged to", 
       "the farmer sowing his corn that kept", 
       "the rooster that crowed in the morn that woke", 
       "the priest all shaven and shorn that married", 
@@ -17,12 +20,22 @@ class House
       "the cat that killed", 
       "the rat that ate", 
       "the malt that lay in", 
-      "the house that Jack built"
+      "the house that Jack built",
     ]
   end
 
-  def line(i)
-      "This is %s.\n" % lines.last(i).join(" ")
-  end
-
+  LINES =
+    [ "the horse and the hound and the horn that belonged to", 
+      "the farmer sowing his corn that kept", 
+      "the rooster that crowed in the morn that woke", 
+      "the priest all shaven and shorn that married", 
+      "the man all tattered and torn that kissed", 
+      "the maiden all forlorn that milked", 
+      "the cow with the crumpled horn that tossed", 
+      "the dog that worried", 
+      "the cat that killed", 
+      "the rat that ate", 
+      "the malt that lay in", 
+      "the house that Jack built",
+    ]
 end
