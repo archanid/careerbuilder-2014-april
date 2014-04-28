a2c195c2d7653103723c816d54f1f0646d98decf
solostyle <spam@solostyle.net>
2014-04-28 12:32:08 -0500

    step

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -9,7 +9,7 @@ class House
     when 1
       "This is %s.\n" % ["the house that Jack built"]
     when 2
-      "This is the malt that lay in %s.\n" % ["the malt that lay in", "the house that Jack built"]
+      "This is %s %s.\n" % ["the malt that lay in", "the house that Jack built"]
       "This is the malt that lay in %s.\n" % ["the house that Jack built"]
     when 3
       "This is the rat that ate the malt that lay in the house that Jack built.\n"
