96a4e79e6e63a513a2b4be712701a6a8e010f71c
solostyle <spam@solostyle.net>
2014-04-28 12:22:07 -0500

    step

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,7 +1,7 @@
 class House
 
 def recite
-  1.upto(12).map {|i| line(i)}.join("\n")
+
 <<-TEXT
 This is the house that Jack built.
 
@@ -27,7 +27,7 @@ This is the farmer sowing his corn that kept the rooster that crowed in the morn
 
 This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
     TEXT
-
+  1.upto(12).map {|i| line(i)}.join("\n")
 end
 
 def line(num)
