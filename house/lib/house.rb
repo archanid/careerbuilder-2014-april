f8b0dc28b00f7f912f9dd94747b0b5e84288d1ff
solostyle <spam@solostyle.net>
2014-04-28 10:58:54 -0500

    refactor tests using %s

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,31 +1,29 @@
 class House
 
-def recite
-  (1..12).map {|i| line(i) }.join("\n")
-end
-
-def line( n )
-  "This is %s" % lines[n]
-
-end
-
-def lines
-  [
-    "the house that Jack built.\n",
-    "the malt that lay in ",
-    "the rat that ate ",
-    "the cat that killed ",
-    "the dog that worried ",
-    "the cow with the crumpled horn that tossed ",
-    "the maiden all forlorn that milked ",
-    "the man all tattered and torn that kissed ",
-    "the priest all shaven and shorn that married ",
-    "the rooster that crowed in the morn that woke ",
-    "the farmer sowing his corn that kept ",
-    "the horse and the hound and the horn that belonged to "
-  ]
+  def recite
+    (1..12).map {|i| line(i) }.join("\n")
   end
 
-end
+  def line( n )
+    "This is %s" % (0...n).to_a.reverse.map {|x| lines[x]}.join
+
+  end
+
+  def lines
+    [
+      "the house that Jack built.\n",
+      "the malt that lay in ",
+      "the rat that ate ",
+      "the cat that killed ",
+      "the dog that worried ",
+      "the cow with the crumpled horn that tossed ",
+      "the maiden all forlorn that milked ",
+      "the man all tattered and torn that kissed ",
+      "the priest all shaven and shorn that married ",
+      "the rooster that crowed in the morn that woke ",
+      "the farmer sowing his corn that kept ",
+      "the horse and the hound and the horn that belonged to "
+    ]
+  end
 
 end
