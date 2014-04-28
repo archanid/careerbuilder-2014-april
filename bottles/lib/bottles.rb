b5a6b29d2194bcc8330f31446cf255187a38faaf
solostyle <spam@solostyle.net>
2014-04-28 15:11:06 -0500

    step

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -2,10 +2,10 @@ class Bottles
 
   def verse(i)
     case i
-    when 1..99
-      "#{amount_starting(i)} #{pluralize(i, container)} #{item_suffix} #{where}, #{i} #{pluralize(i, container)} #{item_suffix}.\n#{second_line_beg(i)}, #{amount_ending(i)} #{pluralize(i-1, container)} #{item_suffix} #{where}.\n"
+    when 0..99
+      "#{amount_starting(i).capitalize} #{pluralize(i, container)} #{item_suffix} #{where}, #{amount_starting(i)} #{pluralize(i, container)} #{item_suffix}.\n#{second_line_beg(i)}, #{amount_ending(i)} #{pluralize(i-1, container)} #{item_suffix} #{where}.\n"
     else
-      "#{amount_starting(i)} #{pluralize(i, container)} #{item_suffix} #{where}, no more #{pluralize(i, container)} #{item_suffix}.\n#{second_line_beg(i)}, #{amount_ending(i)} #{pluralize(i-1, container)} #{item_suffix} #{where}.\n"
+      "#{amount_starting(i).capitalize} #{pluralize(i, container)} #{item_suffix} #{where}, #{amount_starting(i)} #{pluralize(i, container)} #{item_suffix}.\n#{second_line_beg(i)}, #{amount_ending(i)} #{pluralize(i-1, container)} #{item_suffix} #{where}.\n"
     end
   end
 
@@ -19,7 +19,7 @@ class Bottles
 
   def amount_starting(i)
     if i == 0
-      "No more"
+      "no more"
     else
       "#{i}"
     end
