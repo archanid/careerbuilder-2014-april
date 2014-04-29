e86e6e10d2bd00ca0fe31f57c02c9037255cc2ca
solostyle <spam@solostyle.net>
2014-04-29 12:12:59 -0500

    Six pack step: inheritance

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -93,6 +93,12 @@ class Variant
   end
 end
 
+class Variant0 < Variant
+  def current_quantity
+    "no more"
+  end
+end
+
 # first strategy: when 2 and else are the closest to each other, so it would require the smallest difference change. make two changes, first the 2/num, then the bottle/s
 
 # next: collapse them now that they're identical, then propagate changes. collapsing has higher priority. keep going.
