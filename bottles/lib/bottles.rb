669f447438f636820a2862e1b712ddfe3584c5fc
solostyle <spam@solostyle.net>
2014-04-29 09:44:01 -0500

    extract class, what it is, comments

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -14,6 +14,8 @@ class Bottles
     " #{quantity(num-1)} #{ewer(num-1)} of beer on the wall.\n"
   end
 
+  private
+
   def what_to_do(n)
     if n == 0
       "Go to the store and buy some more"
@@ -21,8 +23,6 @@ class Bottles
       "Take #{noun(n)} down and pass it around"
     end
   end
-
-  private
 
   def quantity(n)
     if n == -1
@@ -61,4 +61,6 @@ end
 
 # new requirement: now we want to add six packs of beer!
 
-# what's wrong? we're not fully using single responsibility principle
+# what's wrong? we're not fully using single responsibility principle: all the private methods are more about n than about bottles. this suggests that n ought to be a class and the methods should be class methods for that object. that way this bottles class will be closer to SRP. use the extract class refactoring strategy.
+
+# think about cohesion. Song relies only on verses. verses only relies on verse. verse relies on all the private ones below.
