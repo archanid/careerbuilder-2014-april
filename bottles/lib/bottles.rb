7ff609e20007110a2e4e91bc3f04e2cb0281779c
solostyle <spam@solostyle.net>
2014-04-29 08:41:11 -0500

    group refactor

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -19,7 +19,13 @@ class Bottles
   end
 end
 
-
+def quantity(n)
+  if n == 
+    "no more"
+  else
+    n.to_s
+  end
+end
 
 def noun(n)
   if n == 1
@@ -39,4 +45,4 @@ end
 
 # first strategy: when 2 and else are the closest to each other, so it would require the smallest difference change. make two changes, first the 2/num, then the bottle/s
 
-# next: collapse them now that they're identical, then propagate changes. collapsing has higher priority.
+# next: collapse them now that they're identical, then propagate changes. collapsing has higher priority. keep going.
