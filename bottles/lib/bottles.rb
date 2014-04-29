9d96235621519a8ddd5c95c7f0da47995fbd5d30
solostyle <spam@solostyle.net>
2014-04-29 09:47:31 -0500

    extract class: step

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -51,6 +51,54 @@ class Bottles
   end
 end
 
+class Verse
+
+  def verse(num)
+    "#{quantity(num).capitalize} #{ewer(num)} of beer on the wall," + 
+    " #{quantity(num)} #{ewer(num)} of beer.\n" + 
+    "#{what_to_do(num)}," + 
+    " #{quantity(num-1)} #{ewer(num-1)} of beer on the wall.\n"
+  end
+
+  private
+
+  def what_to_do(n)
+    if n == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{noun(n)} down and pass it around"
+    end
+  end
+
+  def quantity(n)
+    if n == -1
+      99
+    elsif n == 0
+      "no more"
+    else
+      n
+    end.to_s
+  end
+
+  def noun(n)
+    if n == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def ewer(n)
+    if n == 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
+
+end
+
+
 # first strategy: when 2 and else are the closest to each other, so it would require the smallest difference change. make two changes, first the 2/num, then the bottle/s
 
 # next: collapse them now that they're identical, then propagate changes. collapsing has higher priority. keep going.
