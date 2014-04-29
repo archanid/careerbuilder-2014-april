86777ea3dafa00167d6bdbb581e2789f5f70ef5f
solostyle <spam@solostyle.net>
2014-04-29 12:21:26 -0500

    Six pack step

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -105,6 +105,10 @@ class Variant0 < Variant
   def current_quantity
     "no more"
   end
+
+  def remaining_quantity
+    99.to_s
+  end
 end
 
 # first strategy: when 2 and else are the closest to each other, so it would require the smallest difference change. make two changes, first the 2/num, then the bottle/s
