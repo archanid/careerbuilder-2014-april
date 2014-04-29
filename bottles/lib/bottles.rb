6f9fb54bf2ae6c67bec4bcb4bbc189522e0446e5
solostyle <spam@solostyle.net>
2014-04-29 08:24:02 -0500

    group refactor

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -14,9 +14,13 @@ class Bottles
     when 1
       "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+      "#{num} bottles of beer on the wall, #{num} bottles of beer.\nTake one down and pass it around, #{num-1} bottle of beer on the wall.\n"
     else
       "#{num} bottles of beer on the wall, #{num} bottles of beer.\nTake one down and pass it around, #{num-1} bottles of beer on the wall.\n"
     end
   end
 end
+
+# first strategy: when 2 and else are the closest to each other, so it would require the smallest difference change.
+
+# 
