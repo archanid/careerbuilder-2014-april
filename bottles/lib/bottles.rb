2e81b6a0f2b5b1335625b7821a3f710199de5eb8
solostyle <spam@solostyle.net>
2014-04-29 08:37:16 -0500

    group refactor

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -12,7 +12,7 @@ class Bottles
     when 0
       "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+      "#{num} #{ewer(num)} of beer on the wall, #{num} #{ewer(num)} of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     else
       "#{num} #{ewer(num)} of beer on the wall, #{num} #{ewer(num)} of beer.\nTake one down and pass it around, #{num-1} #{ewer(num-1)} of beer on the wall.\n"
     end
