520b6b8b3204c289dcf856968eb68705524956b7
solostyle <spam@solostyle.net>
2014-04-30 09:37:16 -0500

    with chris step

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -54,3 +54,59 @@ class Bottles
     end
   end
 end
+
+
+class Verse
+  attr_reader :num
+
+  def initialize(num)
+  
+  end
+
+  def verse(num)
+    "#{quantity(num).capitalize} #{container(num)} of beer on the wall, " +
+    "#{quantity(num)} #{container(num)} of beer.\n" +
+    "#{action(num)}, " +
+    "#{quantity(num-1)} #{container(num-1)} of beer on the wall.\n"
+  end
+
+  private
+
+  def quantity(num)
+    case num
+    when -1
+      99.to_s
+    when 0
+      'no more'
+    else
+      num.to_s
+    end
+  end
+
+  def container(num)
+    case num
+    when 1
+      'bottle'
+    else
+      'bottles'
+    end
+  end
+
+  def action(num)
+    case num
+    when 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun(num)} down and pass it around"
+    end
+  end
+
+  def pronoun(num)
+    case num
+    when 1
+      'it'
+    else
+      'one'
+    end
+  end
+end
