fd32687db60937f781fd1b3f9208d6ccfa4aa122
solostyle <spam@solostyle.net>
2014-05-01 15:54:11 -0500

    put a magic number back in to eliminate duplication

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,7 +1,7 @@
 class House
 
   def recite
-    1.upto(segments.length).map {|i| line(i)}.join("\n")
+    1.upto(12).map {|i| line(i)}.join("\n")
   end
 
   def random_recite
@@ -54,20 +54,5 @@ class House
       "the cat that killed",
       "the rat that ate", 
       "the malt that lay in"]
-  end
-
-  def segments
-    [ "the horse and the hound and the horn that belonged to",
-      "the farmer sowing his corn that kept",
-      "the rooster that crowed in the morn that woke",
-      "the priest all shaven and shorn that married",
-      "the man all tattered and torn that kissed",
-      "the maiden all forlorn that milked",
-      "the cow with the crumpled horn that tossed",
-      "the dog that worried",
-      "the cat that killed",
-      "the rat that ate", 
-      "the malt that lay in", 
-      "the house that Jack built"]
   end
 end
