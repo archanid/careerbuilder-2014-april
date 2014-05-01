7c90bc7221d59485b1060c78c1ae7abdb7cdca0d
solostyle <spam@solostyle.net>
2014-05-01 14:41:49 -0500

    does ths work?

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -5,6 +5,7 @@ class House
   end
 
   def random_recite
+    1.upto(11).map {|i| random_line(i)}.join("\n")
   end
 
   def random_line
@@ -18,7 +19,7 @@ class House
   private
 
   def random_lines
-    (0..segments.length-2).map {|i| segments[i]}.join(" ")
+    (0..random_segments.length-1).map {|i| segments[i]}.join(" ")
   end
 
   def segments
@@ -34,5 +35,19 @@ class House
       "the rat that ate", 
       "the malt that lay in", 
       "the house that Jack built"]
+  end
+
+  def random_segments
+    [ "the horse and the hound and the horn that belonged to",
+      "the farmer sowing his corn that kept",
+      "the rooster that crowed in the morn that woke",
+      "the priest all shaven and shorn that married",
+      "the man all tattered and torn that kissed",
+      "the maiden all forlorn that milked",
+      "the cow with the crumpled horn that tossed",
+      "the dog that worried",
+      "the cat that killed",
+      "the rat that ate", 
+      "the malt that lay in"]
   end
 end
