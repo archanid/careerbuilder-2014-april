cb18f93139a64241046e14bd0970cf8f1f39660b
solostyle <spam@solostyle.net>
2014-05-01 15:44:29 -0500

    this is so ugly though

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -5,15 +5,19 @@ class House
   end
 
   def random_recite
-    1.upto(random_segments.length).map {|i| random_line(i)}.join("\n")
+    1.upto(segments_minus_one.length).map {|i| random_line(i)}.join("\n")
   end
 
   def line(num)
-    "This is %s%s.\n" % [segments_minus_one.last(num-1).join, segments.last(1).join(" ")]
+    if num == 1
+      "This is %s.\n" % [last_phrase]
+    else
+      "This is %s %s.\n" % [segments_minus_one.last(num-1).join(" "), last_phrase]
+    end
   end
   
   def random_line(num)
-    "This is %s %s.\n" % [randomize_segments(num), segments.last(1).join(" ")]
+    "This is %s %s.\n" % [randomize_segments(num), last_phrase]
   end
 
   private
@@ -26,18 +30,22 @@ class House
     lower_bound.upto(upper_bound).map {|i| i}.shuffle
   end
 
+  def last_phrase
+    "the house that Jack built"
+  end
+
   def segments_minus_one
-    [ "the horse and the hound and the horn that belonged to ",
-      "the farmer sowing his corn that kept ",
-      "the rooster that crowed in the morn that woke ",
-      "the priest all shaven and shorn that married ",
-      "the man all tattered and torn that kissed ",
-      "the maiden all forlorn that milked ",
-      "the cow with the crumpled horn that tossed ",
-      "the dog that worried ",
-      "the cat that killed ",
-      "the rat that ate ", 
-      "the malt that lay in "]
+    [ "the horse and the hound and the horn that belonged to",
+      "the farmer sowing his corn that kept",
+      "the rooster that crowed in the morn that woke",
+      "the priest all shaven and shorn that married",
+      "the man all tattered and torn that kissed",
+      "the maiden all forlorn that milked",
+      "the cow with the crumpled horn that tossed",
+      "the dog that worried",
+      "the cat that killed",
+      "the rat that ate", 
+      "the malt that lay in"]
   end
 
   def segments
@@ -53,19 +61,5 @@ class House
       "the rat that ate", 
       "the malt that lay in", 
       "the house that Jack built"]
-  end
-
-  def random_segments
-    [ "the horse and the hound and the horn that belonged to",
-      "the farmer sowing his corn that kept",
-      "the rooster that crowed in the morn that woke",
-      "the priest all shaven and shorn that married",
-      "the man all tattered and torn that kissed",
-      "the maiden all forlorn that milked",
-      "the cow with the crumpled horn that tossed",
-      "the dog that worried",
-      "the cat that killed",
-      "the rat that ate", 
-      "the malt that lay in"]
   end
 end
