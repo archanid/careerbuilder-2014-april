311ab5ab6af4bb037610c8a80a7a560f42957e10
solostyle <spam@solostyle.net>
2014-05-01 15:53:14 -0500

    aha. shuffle the segments, not just some array

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -32,11 +32,6 @@ class House
 
   def randomize_segments(num)
     shuffled_segments.last(num-1).join(" ")
-    shuffled_array(0, num-2).map {|i| segments[i]}.join(" ")
-  end
-
-  def shuffled_array(lower_bound, upper_bound)
-    lower_bound.upto(upper_bound).map {|i| i}.shuffle
   end
 
   def shuffled_segments
