7eba6ee864a55c0db11c8dd2bec74f3418230e8e
solostyle <spam@solostyle.net>
2014-05-02 16:08:18 -0500

    eliminate duplication

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -9,47 +9,11 @@ class House
   end
 
   def line(num)
-    if num == 1
-      "This is %s.\n" % [Foo.last_phrase]
-    else
-      "This is %s %s.\n" % [Foo.sequence_segments(num), Foo.last_phrase]
-    end
+    Foo.line(num)
   end
   
   def random_line(num)
-    if num == 1
-      "This is %s.\n" % [Foo.last_phrase]
-    else
-      "This is %s %s.\n" % [Foo.randomize_segments(num), Foo.last_phrase]
-    end
-  end
-
-  private
-
-  def sequence_segments(num)
-    segments.last(num-1).join(" ")
-  end
-
-  def randomize_segments(num)
-    segments.shuffle.last(num-1).join(" ")
-  end
-
-  def last_phrase
-    "the house that Jack built"
-  end
-
-  def segments
-    [ "the horse and the hound and the horn that belonged to",
-      "the farmer sowing his corn that kept",
-      "the rooster that crowed in the morn that woke",
-      "the priest all shaven and shorn that married",
-      "the man all tattered and torn that kissed",
-      "the maiden all forlorn that milked",
-      "the cow with the crumpled horn that tossed",
-      "the dog that worried",
-      "the cat that killed",
-      "the rat that ate", 
-      "the malt that lay in"]
+    Foo.random_line(num)
   end
 end
 
