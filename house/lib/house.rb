f1862300b724b73a85f596d3be974c1dc93a9b81
solostyle <spam@solostyle.net>
2014-05-11 14:59:41 -0500

    Not totally green, but this is a simpler place to restart refactoring.

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,72 +1,29 @@
 class House
+
   def recite
-    1.upto(12).map {|i| 
-      case i
-      when 1
-        Variant1
-      else
-        Variant
-      end.new(i).line
-    }.join("\n")
+    1.upto(12).map {|i| line(i)}.join("\n")
   end
 
   def random_recite
-    1.upto(11).map {|i| 
-      case i
-      when 1
-        Variant1
-      else
-        Variant
-      end.new(i).random_line
-    }.join("\n")
+    1.upto(11).map {|i| random_line(i)}.join("\n")
   end
 
-  def line(n)
-    case n
-    when 1
-      Variant1
-    else
-      Variant
-    end.new(n).line
+  def line(num)
+    "This is %s.\n" % segments.last(num).join(" ")
   end
   
-  def random_line(n)
-    case n
-    when 1
-      Variant1
-    else
-      Variant
-    end.new(n).random_line
-  end
-end
-
-class Variant
-  attr_reader :num
-
-  def initialize(num)
-    @num = num
-  end
-
-  def line
-    "This is %s %s.\n" % [sequence_segments, last_phrase]
-  end
-  
-  def random_line
-    "This is %s %s.\n" % [randomize_segments, last_phrase]
-  end
-
-  def sequence_segments
-    segments.last(num-1).join(" ")
-  end
-
-  def randomize_segments
-    segments.shuffle.last(num-1).join(" ")
+  def random_line(num)
+    "This is %s %s.\n" % [random_lines(num), segments.last(1).join(" ")]
   end
 
   private
 
-  def last_phrase
-    "the house that Jack built"
+  def random_lines(num)
+    shuffled_array(0, num-2).map {|i| segments[i]}.join(" ")
+  end
+
+  def shuffled_array(lower_bound, upper_bound)
+    lower_bound.upto(upper_bound).map {|i| i}.shuffle
   end
 
   def segments
@@ -79,17 +36,22 @@ class Variant
       "the cow with the crumpled horn that tossed",
       "the dog that worried",
       "the cat that killed",
-      "the rat that ate", 
+      "the rat that ate",
+      "the malt that lay in",
+      "the house that Jack built"]
+  end
+
+  def random_segments
+    [ "the horse and the hound and the horn that belonged to",
+      "the farmer sowing his corn that kept",
+      "the rooster that crowed in the morn that woke",
+      "the priest all shaven and shorn that married",
+      "the man all tattered and torn that kissed",
+      "the maiden all forlorn that milked",
+      "the cow with the crumpled horn that tossed",
+      "the dog that worried",
+      "the cat that killed",
+      "the rat that ate",
       "the malt that lay in"]
-  end
-end
-
-class Variant1 < Variant
-  def line
-    "This is %s.\n" % [last_phrase]
-  end
-
-  def random_line
-    "This is %s.\n" % [last_phrase]
   end
 end
